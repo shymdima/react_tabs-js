87a2080e405c0149ccca66f20d5f632be3a9af05
dima <shymdima74@gmail.com>
2024-12-19 11:52:36 +0200

    addtask solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,36 @@
-export const Tabs = () => {};
+import classNames from "classnames";
+export const Tabs = ({tabs, activeTabId, onTabSelected, setOnTabSelected, setActiveTabId}) => {
+    let i = 0;
+    return (
+        <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            
+            return (
+                <li className={classNames({'is-active' : activeTabId === tab.id})} 
+                data-cy="Tab"
+                onClick={ () => {
+                    setOnTabSelected(tab)
+                    setActiveTabId(tab.id)
+                }}
+                key={tab.id}
+                >
+                    <a href={`#${activeTabId.id}`} data-cy="TabLink">
+                    {tab.title}
+                    </a>
+                </li> 
+            )
+        })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {onTabSelected.content}
+      </div>
+    </div>
+    )
+};
+
+
+          
